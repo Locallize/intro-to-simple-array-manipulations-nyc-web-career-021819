abd3993c91b903aa4d4134faa2f09c391dfecea5
Fillan Lee <fillanlee@aol.com>
2019-02-06 04:34:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -36,16 +36,14 @@ array.uniq
 end
  
 def using_flatten(array)
-
+  array.flatten
 end
-  takes in an argument of an array that contains other arrays and uses the flatten method to return an array of strings (FAILED - 17)
-
-def using_delete
-
+ 
+def using_delete(array, string)
+array.delete(string)
 end
-  takes in two arguments, an array and a string, and uses the delete method to remove any items from the array that are equal to that string (FAILED - 18)
 
-def using_delete_at
-
+def using_delete_at(array, int)
+array.delete(int)
 end
-  takes in two arguments, an array and an integer and deletes the element at the index of the array that is equal to that integer (FAILED - 19)
+ 
